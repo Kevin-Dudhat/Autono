a81b64b20f136cc7907883c978a1001bd5c41ab3
Kevin Dudhat <kevindudhat654@gmail.com>
2023-12-11 22:01:32 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './css/style.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Nav from './Components/Nav'
+import Home from './Components/Home';
+import About from './Components/About';
+import Career from './Components/Career';
+import Technology from './Components/Technology';
+import Footer from './Components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/technology" element={<Technology />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/career" element={<Career />} />
+          </Routes>
+          <Footer/>
+        {/* <Nav/>
+        <Home/> */}
+
+      </div>
+    );
+  }
 }
 
 export default App;
